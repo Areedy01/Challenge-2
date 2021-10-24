2d4c405eb7b8d6438627d9c20a019eccc47b95ba
Miryxx <alexisreedy@gmail.com>
2021-10-24 14:23:06 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
+
+    Animator anim;
     public float speed;
 
     public Text score;
     public Text lives;
     public GameObject winText;
     public GameObject loseText;
+
+    private bool facingRight = true;
 
     private bool gameOver;
     private int scoreValue = 0;
@@ -21,12 +25,19 @@ public class PlayerScript : MonoBehaviour
     public AudioClip winning;
     public AudioSource musicSource;
 
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
         lives.text = livesValue.ToString();
+
+        anim = GetComponent<Animator>();
 
         musicSource.clip = background;
         musicSource.Play();
@@ -44,6 +55,35 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        if (hozMovement < 0 || hozMovement > 0)
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (hozMovement == 0)
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (vertMovement < 0 && isOnGround == true)
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (vertMovement > 0 && isOnGround == false)
+        {
+            anim.SetInteger("State", 2);
+        }
 
         if (scoreValue >= 8 && gameOver == false)
         {
@@ -94,12 +134,20 @@ public class PlayerScript : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 2), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
+                rd2d.AddForce(new Vector2(0, 3f), ForceMode2D.Impulse);
             }
         }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
